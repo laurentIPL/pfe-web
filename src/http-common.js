ea33e78428d8e32798be9a6c4074ca49cb8ccb69
laurentIPL <73901816+laurentIPL@users.noreply.github.com>
2020-12-10 18:49:42 +0100

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://pfeapi.herokuapp.com/api",
+  baseURL: "https://pfeapi-prod.herokuapp.com/api",
   //baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json",
